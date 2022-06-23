c124eff93c12f82a3b79a3c1383479195ac0519d
SKindij <serhii.kindij@gmail.com>
2022-06-23 23:02:49 +0300

    about objects and arrays

--- js/script-PI.js
+++ js/script-PI.js
@@ -30,6 +30,9 @@ let svinAges = [41, 38, 18];
 console.log(svinFamily[2], svinAges[2]);
 // SynSvin 18
 console.log(svinFamily);
-// 
+// Array(3)
 
+Zhabka.height = 175;
+console.log(Zhabka);
+// Object
 
