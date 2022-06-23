2032e9da5e013d73556d1200d53d2079774c6af1
SKindij <serhii.kindij@gmail.com>
2022-06-23 23:13:36 +0300

    added new object

--- js/script-PI.js
+++ js/script-PI.js
@@ -30,9 +30,21 @@ let svinAges = [41, 38, 18];
 console.log(svinFamily[2], svinAges[2]);
 // SynSvin 18
 console.log(svinFamily);
-// Array(3)
+// Array(3) ['Tatko', 'Matushia', 'SynSvin']
 
 Zhabka.height = 175;
+Zhabka['from'] = 'Pryluki';
 console.log(Zhabka);
-// Object
+// Object {name: 'Tanusha', age: 38, isPupsik: true, height: 175, from: 'Pryluki'}
 
+let SvinFather = {
+    name: 'Serhii',
+    age: 41,
+    isPupsik: false,
+    frontEnd: {
+        html: true,
+        css: true,
+        js: true
+    },
+};
+console.log(SvinFather);
