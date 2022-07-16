b50addedbde98927e5b2abcf16ccac7666f07966
SKindij <serhii.kindij@gmail.com>
2022-07-16 20:26:29 +0300

    apdated film lesson

--- js/films-project.js
+++ js/films-project.js
@@ -9,14 +9,34 @@ const personalMovieDB = {
     privat: false
 };
 
-const aq = prompt('One of the last movies you watched?', ''),
-      bq = prompt('How much would you rate it?', ''),
-      cq = prompt('One of the last movies you watched?', ''),
-      dq = prompt('How much would you rate it?', '');
+for (let i = 0; i < 2; i++) {
+    const aq = prompt('One of the last movies you watched?', ''),
+          bq = prompt('How much would you rate it?', '');
 
-      personalMovieDB.movies[aq] = bq;
-      personalMovieDB.movies[cq] = dq;
-      
-      console.log(personalMovieDB);
+    if (aq != null && bq != null && aq != '' && bq != '' && aq.length < 50) {
+        personalMovieDB.movies[aq] = bq;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Watched quite a few films");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("You are a classic spectator");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Are you a cinephile");
+} else {
+    console.log("An error has occurred");
+}
+
+console.log(personalMovieDB);
+
+
+
+
+
 
 
