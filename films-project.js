903d7b5b592886fb9c1a78c530343a015439f463
SKindij <serhii.kindij@gmail.com>
2022-07-16 13:02:02 +0300

    starting with new project

--- films-project.js
+++ films-project.js
@@ -1,9 +1,18 @@
 'use strict';
+const numberOfFilms = +prompt('How many films have you already watched?', '');
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-
-
-
+const aq = prompt('One of the last movies you watched?', ''),
+      bq = prompt('How much would you rate it?', ''),
+      cq = prompt('One of the last movies you watched?', ''),
+      dq = prompt('How much would you rate it?', '');
 
 
 
