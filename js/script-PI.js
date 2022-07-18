e68c7a61a510cc95aa6a45704d55daab8e0e105a
SKindij <serhii.kindij@gmail.com>
2022-07-18 21:10:22 +0300

    course tasks junior

--- js/script-PI.js
+++ js/script-PI.js
@@ -515,3 +515,48 @@ console.log(newClonFamily) // => ['Tatko', 'Matushia', 'SynSvin']
 const newClonPupsik = {...addPupsik};
 console.log(newClonPupsik) // => {isPupsik: true, fromSvinFamily: true}
 
+//course tasks junior
+const personalPlanPeter = {
+  name: "Peter",
+  age: "29",
+  skills: {
+      languages: ['ru', 'eng'],
+      programmingLangs: {
+          js: '20%',
+          php: '10%'
+      },
+      exp: '1 month'
+  },
+  showAgeAndLangs: function(plan) {
+      const {age} = plan;
+      const {languages} = plan.skills;
+      let str = `Мне ${age} и я владею языками: `;
+
+      languages.forEach(function(lang) {
+          str += `${lang.toUpperCase()} `;
+      });
+
+      return str;
+  }
+};
+
+personalPlanPeter.showAgeAndLangs(personalPlanPeter);
+
+function showExperience(plan) {
+  const {exp} = plan.skills;
+  return exp;
+}
+console.log(showExperience(personalPlanPeter));
+
+function showProgrammingLangs(plan) {
+  let str = '';
+  const {programmingLangs} = plan.skills;
+  for (let key in programmingLangs) {
+      str += `Язык ${key} изучен на ${programmingLangs[key]}\n`
+  }
+
+  return str;
+}
+console.log(showProgrammingLangs(personalPlanPeter));
+
+
