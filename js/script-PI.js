fb1a0f2977c4136a9d98f5d1532dae5f5990d45f
SKindij <serhii.kindij@gmail.com>
2022-07-19 22:06:48 +0300

    shoppingMallData

--- js/script-PI.js
+++ js/script-PI.js
@@ -634,6 +634,47 @@ console.log(soldierRufus.weapon); // => rifle
 
 const soldierJenifer = Object.create(soldier);
 
+//course tasks middle
+const shoppingMallData = {
+  shops: [
+      {
+          width: 10,
+          length: 5
+      },
+      {
+          width: 15,
+          length: 7
+      },
+      {
+          width: 20,
+          length: 5
+      },
+      {
+          width: 8,
+          length: 10
+      }
+  ],
+  height: 5,
+  moneyPer1m3: 30,
+  budget: 50000
+}
+
+function isBudgetEnough(data) {
+  let square = 0;
+  let volume = 0;
+  data.shops.forEach(shop => {
+      square += shop.width * shop.length;
+  });
+  volume = data.height * square;
+  if (data.budget - (volume * data.moneyPer1m3) >= 0) {
+      return 'Enough budget';
+  } else {
+      return 'Budget is not enough';
+  }
+}
+console.log(isBudgetEnough(shoppingMallData)); // => 
+
+
 
 
 
