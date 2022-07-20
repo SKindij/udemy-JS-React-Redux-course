4ecaa8b8795d7ff64b3090030ec2e6506d958d01
SKindij <serhii.kindij@gmail.com>
2022-07-20 21:18:12 +0300

    Create a list of movies on the page

--- DB-film-project/js/script.js
+++ DB-film-project/js/script.js
@@ -1,26 +1,41 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
 const movieDB = {
     movies: [
         "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
+        "Ліга Справедливості",
+        "Ла-ла ленд",
+        "Одержимість",
+        "Скотт Пілігрим проти..."
     ]
 };
+
+const adv = document.querySelectorAll('.promo__adv img'),
+    poster = document.querySelector('.promo__bg'),
+    genre = poster.querySelector('.promo__genre'),
+    movieList = document.querySelector('.promo__interactive-list');
+// Remove all ad units from the page (right side of the site)
+adv.forEach(item => {
+    item.remove();
+});
+// Change movie genre => "comedy" to "drama"
+genre.textContent = 'драма';
+// Change movie poster background to image
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+// Create a list of movies on the page based on the data from this JS file.
+movieList.innerHTML = "";
+movieDB.movies.sort();
+// Add numbering of output films
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1} ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
+
+
+
+
+
 
