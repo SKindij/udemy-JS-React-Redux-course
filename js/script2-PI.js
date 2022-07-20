83479ffab482a3e577590c7ab0da609b3be19ae1
SKindij <serhii.kindij@gmail.com>
2022-07-20 23:41:59 +0300

    working with DOM events

--- js/script2-PI.js
+++ js/script2-PI.js
@@ -1,8 +1,19 @@
 'use strict'
 console.log('JS-FrontEnd-Udemy-PI');
 
+const leftButton = document.querySelector('#leftButton');
+let info = false;
+const elemInfo = (event) => {
+    console.log(event.target);
+    console.log(event.type);
+    info = true;
+    if (info) {
+      leftButton.removeEventListener('click', elemInfo)
+    };
+};
+leftButton.addEventListener('click', elemInfo);
 
-
+const firstLink = document.querySelector('#firstLink');
 
 
 
