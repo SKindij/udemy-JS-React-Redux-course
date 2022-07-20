9eba2d46ccf661f48ebc69179eb4a32c7ec7fcf4
SKindij <serhii.kindij@gmail.com>
2022-07-20 23:58:06 +0300

    function on click

--- js/script2-PI.js
+++ js/script2-PI.js
@@ -2,6 +2,8 @@
 console.log('JS-FrontEnd-Udemy-PI');
 
 const leftButton = document.querySelector('#leftButton');
+const allButtons = document.querySelectorAll('button');
+
 let info = false;
 const elemInfo = (event) => {
     console.log(event.target);
@@ -14,6 +16,19 @@ const elemInfo = (event) => {
 leftButton.addEventListener('click', elemInfo);
 
 const firstLink = document.querySelector('#firstLink');
+firstLink.addEventListener('click', function(event) {
+  event.preventDefault(); // override default browser behavior
+  console.log(event.target);
+});
+
+const showInfo = (event) => {
+  console.log('you ' + event.type + ' on ' + event.target);
+
+};
+
+allButtons.forEach(btn => {
+  btn.addEventListener('click', showInfo, {once: true});
+});
 
 
 
