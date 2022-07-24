b8175917cf71ace31ca856498dfbfcc6a99b27b7
SKindij <serhii.kindij@gmail.com>
2022-07-24 14:55:22 +0300

    working with modal window

--- FOOD-project/js/food.js
+++ FOOD-project/js/food.js
@@ -36,7 +36,7 @@ tabsParent.addEventListener('click', function(event) {
   });
   
   // Timer
-  const deadline = '2022-06-11';
+  const deadline = '2022-08-01';
   function getTimeRemaining(endtime) {
       const t = Date.parse(endtime) - Date.parse(new Date()),
           days = Math.floor( (t/(1000*60*60*24)) ),
@@ -81,5 +81,41 @@ tabsParent.addEventListener('click', function(event) {
   }
 
   setClock('.timer', deadline);
+
+  // Modal
+  const modalTrigger = document.querySelectorAll('[data-modal]'),
+  modal = document.querySelector('.modal'),
+  modalCloseBtn = document.querySelector('[data-close]');
+
+modalTrigger.forEach(btn => {
+  btn.addEventListener('click', function() {
+      modal.classList.add('show');
+      modal.classList.remove('hide');
+      // Либо вариант с toggle - но тогда назначить класс в верстке
+      document.body.style.overflow = 'hidden';
+  });
+});
+
+function closeModal() {
+  modal.classList.add('hide');
+  modal.classList.remove('show');
+  // Либо вариант с toggle - но тогда назначить класс в верстке
+  document.body.style.overflow = '';
+}
+
+modalCloseBtn.addEventListener('click', closeModal);
+
+modal.addEventListener('click', (e) => {
+  if (e.target === modal) {
+      closeModal();
+  }
+});
+
+document.addEventListener('keydown', (e) => {
+  if (e.code === "Escape" && modal.classList.contains('show')) { 
+      closeModal();
+  }
+});
+
 });
 
