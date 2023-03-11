40b8502d2e41a0d041330c5fe8d2b4c2d3f86883
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-03-11 17:14:00 +0200

    Update array-task.js

--- Edvanced-JS/tasks/array-task.js
+++ Edvanced-JS/tasks/array-task.js
@@ -19,12 +19,32 @@ const films = [
         rating: 4
     }
 ];
+
 /* Write function that will take this array and return an array of objects 
 with only those movies that have a rating greater than or equal to 8. */
 function showGoodFilms(arr) {
     return arr.filter( film => film.rating >= 8 );
 };
 showGoodFilms(films); // => [ {"name":"Titanic", "rating":9}, {"name": "Matrix","rating": 8} ]
+
+// Write function that will take same array and return a string that contains names of movies separated by commas
+/*
+function showListOfFilms(arr) {
+    return arr.reduce( (acc, curr) => `${typeof(acc) === 'object' ? acc.name : acc}, ${curr.name}` );
+}
+*/
+function showListOfFilms(movies) {
+    const names = movies.reduce( (accumulator, currentValue) => {
+        return accumulator + (accumulator.length > 0 ? ', ' : '') + currentValue.name;
+      }, '');
+    return names;
+}
+showListOfFilms(films); // => 'Titanic, Die hard 5, Matrix, Some bad film'
+
+
+
+
+
 
 
 
