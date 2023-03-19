741eacae0b78b27b702206cf7196abc8bf97ddb1
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-03-19 17:49:14 +0200

    try => catch (error)

--- Edvanced-JS/preview/script.js
+++ Edvanced-JS/preview/script.js
@@ -15,8 +15,25 @@ const data = [
     }
 ]
 
-data.forEach(blockObj => {
-    const block = document.createElement(blockObj.tag);
-    block.setAttribute('id', blockObj.id);
-    document.body.append(block);
-})
+try {
+    data.forEach( (blockObj, i) => {
+        const block = document.createElement(blockObj.tag);
+    
+        if(!blockObj.id) throw new SyntaxError(`In the data under no ${i+1} there is no id`);
+    
+        block.setAttribute('id', blockObj.id);
+        document.body.append(block);
+    })
+} catch (error) {
+    console.error(error.name);
+    console.log(error.message);
+    console.log(error.stack);
+}
+
+
+console.log('enother examples');
+const errDef = new Error('some defoult message');
+const errSyn = new SyntaxError('some syntax message');
+
+console.log(errDef.name, errDef.message, errDef.stack);
+console.log(errSyn.name, errSyn.message, errSyn.stack);
