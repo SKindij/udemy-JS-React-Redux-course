c956262d851b33a2719ac2619b6f00d511b5d8bf
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-03-23 22:04:59 +0200

    Update animation.js

--- Edvanced-JS/Animation/animation.js
+++ Edvanced-JS/Animation/animation.js
@@ -4,3 +4,19 @@ const btnPhone = document.querySelector('#iphone'),
       btnMacbook = document.querySelector('#macbook'),
       images = document.querySelectorAll('img');
 
+let phoneAnimation;      
+
+btnPhone.addEventListener('click', () => {
+    if (!phoneAnimation) {
+        phoneAnimation = images[0].animate([
+            {transform: 'translateY(0)'},
+            {transform: 'translateY(100px)'},
+            {transform: 'translateY(-100px)'},
+            {transform: 'translateY(0)'}
+        ], {
+            duration: 3000,
+            iterations: Infinity
+        });
+    }
+})
+
