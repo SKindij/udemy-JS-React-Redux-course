94beb60cddd5572a0d3c51cc02386309709604c2
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-03-24 09:54:24 +0200

    Create event-loop.js

---
+++ Edvanced-JS/event-loop.js
@@ -0,0 +1,14 @@
+'use strict'
+
+console.log(1);
+
+setTimeout(() => {
+    console.log('time out 2 sec');
+}, 2000);
+
+setTimeout(() => {
+    console.log('time out 4 sec');
+}, 4000);
+
+console.log(2);
+
