236f85b76fb3fcfd23cc1645cae25b129b63d6d2
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-03-26 18:23:41 +0300

    Create goods-add-form.js

---
+++ REACT/lowest-price/src/components/goods-add-form/goods-add-form.js
@@ -0,0 +1,16 @@
+import './goods-add-form.css';
+
+const GoodsAddForm = () => {
+  return (
+    <div className="app-add-form">
+      <h3>Додайте нову позицію</h3>
+      <form className="add-form d-flex">
+        <input type="text" className="form-control new-post-label" placeholder="Назва товару?" />
+        <input type="number" className="form-control new-post-label" placeholder="Ціна в UAH?" />
+        <button type="submit" className="btn btn-outline-light">Додати</button>
+      </form>
+    </div>
+  )
+}
+
+export default GoodsAddForm;
