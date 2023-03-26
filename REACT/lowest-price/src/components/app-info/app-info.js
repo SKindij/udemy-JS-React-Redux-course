b2c9bb5e071a1fdaca296709e70ff52cb50bf329
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-03-26 18:18:31 +0300

    Create app-info.js

---
+++ REACT/lowest-price/src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import "./app-info.css";
+
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h1>Облік акційних цін у магазинах</h1>
+      <h2>Загальна кількість товарів: </h2>
+      <h2>Кращий особистий вибір:</h2>
+    </div>
+  )
+}
+
+export default AppInfo;
