f61dbdbab0643a16a63cb9c482bb2f909b7fe852
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-03-26 18:26:08 +0300

    Create goods-list-item.js

---
+++ REACT/lowest-price/src/components/goods-list-item/goods-list-item.js
@@ -0,0 +1,17 @@
+import './goods-list-item.css';
+
+const GoodsListItem = () => {
+  return (
+    <li className="list-group-item d-flex justify-content-between">
+      <span className="list-group-item-label">Colonist Portwein</span>
+      <input type="text" className="list-group-item-input" defaultValue="510₴"/>
+      <div className='d-flex justify-content-center align-items-center'>
+        <button type="button" className="btn-cookie btn-sm"><i className="fas fa-cookie"></i></button>
+        <button type="button" className="btn-trash btn-sm"><i className="fas fa-trash"></i></button>
+          <i className="fas fa-star"></i>
+      </div>
+    </li>
+  )
+}
+
+export default GoodsListItem;
