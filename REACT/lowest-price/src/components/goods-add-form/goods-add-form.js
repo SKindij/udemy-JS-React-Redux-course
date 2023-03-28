f54117fe24f7e202269c0274f84dadb953ac1317
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-03-28 23:48:48 +0300

    Update goods-add-form.js

--- REACT/lowest-price/src/components/goods-add-form/goods-add-form.js
+++ REACT/lowest-price/src/components/goods-add-form/goods-add-form.js
@@ -1,5 +1,6 @@
+import { Component } from 'react';
 import './goods-add-form.css';
-
+/*
 const GoodsAddForm = () => {
   return (
     <div className="app-add-form">
@@ -11,6 +12,49 @@ const GoodsAddForm = () => {
       </form>
     </div>
   )
+}
+*/
+class GoodsAddForm extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          brand: '',
+          price: ''
+      }
+  }
+  onValueChange = (e) => {
+      this.setState({
+          [e.target.name] : e.target.value
+      })
+  }
+
+  render() {
+      const {brand, type, volume, price} = this.state;
+
+      return (
+          <div className="app-add-form">
+              <h3>Додайте нову позицію</h3>
+              <form
+                  className="add-form d-flex">
+                  <input type="text"
+                      className="form-control new-post-label"
+                      placeholder="товар різновид міра"
+                      name="brand"
+                      value={[brand, type, volume]} 
+                      onChange={this.onValueChange}/>
+                  <input type="number"
+                      className="form-control new-post-label"
+                      placeholder="Ціна в UAH?"
+                      name="price"
+                      value={price} 
+                      onChange={this.onValueChange}/>
+  
+                  <button type="submit"
+                          className="btn btn-outline-light">Додати</button>
+              </form>
+          </div>
+      )
+  }
 }
 
 export default GoodsAddForm;
