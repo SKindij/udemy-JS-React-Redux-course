b3fa3c66e286883083ff026d9e2966af944ea5b7
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-03-28 23:35:57 +0300

    Update goods-add-form.js

--- REACT/lowest-price/src/components/goods-add-form/goods-add-form.js
+++ REACT/lowest-price/src/components/goods-add-form/goods-add-form.js
@@ -5,7 +5,7 @@ const GoodsAddForm = () => {
     <div className="app-add-form">
       <h3>Додайте нову позицію</h3>
       <form className="add-form d-flex">
-        <input type="text" className="form-control new-post-label" placeholder="Назва товару?" />
+        <input type="text" className="form-control new-post-label" placeholder="товар різновид міра" />
         <input type="number" className="form-control new-post-label" placeholder="Ціна в UAH?" />
         <button type="submit" className="btn btn-outline-light">Додати</button>
       </form>
