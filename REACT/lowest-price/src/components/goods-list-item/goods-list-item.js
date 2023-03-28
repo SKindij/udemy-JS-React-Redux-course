4cde36a916ee50dc13075b2fe846897cee0e3d34
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-03-28 23:28:24 +0300

    Update goods-list-item.js

--- REACT/lowest-price/src/components/goods-list-item/goods-list-item.js
+++ REACT/lowest-price/src/components/goods-list-item/goods-list-item.js
@@ -1,17 +1,48 @@
+import { Component } from 'react';
 import './goods-list-item.css';
 
-const GoodsListItem = () => {
-  return (
-    <li className="list-group-item d-flex justify-content-between">
-      <span className="list-group-item-label">Colonist Portwein</span>
-      <input type="text" className="list-group-item-input" defaultValue="510₴"/>
-      <div className='d-flex justify-content-center align-items-center'>
-        <button type="button" className="btn-cookie btn-sm"><i className="fas fa-cookie"></i></button>
-        <button type="button" className="btn-trash btn-sm"><i className="fas fa-trash"></i></button>
+class GoodsListItem extends Component{
+  constructor(props) {
+      super(props);
+      this.state = {
+          wish: false,
+          star: false
+      }
+  }
+  onWish = () => {
+    // in callback, we have to rely on previous state of element
+      this.setState( ({wish}) => ({
+          wish: !wish
+      }) )
+  }
+  onStar = () => {
+    // in callback, we have to rely on previous state of element   
+      this.setState( ({star}) => ({
+          star: !star
+      }) )
+  }
+
+  render() {
+    const {brand, type, volume, price} = this.props;
+    const {wish, star} = this.state;
+
+    let classNames = "list-group-item d-flex justify-content-between";
+      if (wish) { classNames += ' wish'; };
+      if (star) { classNames += ' like'; };
+  
+    return (
+      <li className={classNames}>
+        <span className="list-group-item-label" onClick={this.onStar}>{brand} {type} {volume}L</span>
+        <input type="text" className="list-group-item-input" defaultValue={price + ' ₴'}/>
+        <div className='d-flex justify-content-center align-items-center'>
+          <button type="button" className="btn-cookie btn-sm" onClick={this.onWish}><i className="fas fa-cookie"></i></button>
+          <button type="button" className="btn-trash btn-sm"><i className="fas fa-trash"></i></button>
           <i className="fas fa-star"></i>
-      </div>
-    </li>
-  )
+        </div>
+      </li>
+    )
+  }
 }
+
 
 export default GoodsListItem;
