89b3dd55f0b2a58e6da0c1e69d0dae368288b246
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-03-30 22:05:09 +0300

    Update goods-list.js

--- REACT/lowest-price/src/components/goods-list/goods-list.js
+++ REACT/lowest-price/src/components/goods-list/goods-list.js
@@ -2,14 +2,18 @@ import GoodsListItem from "../goods-list-item/goods-list-item";
 
 import './goods-list.css';
 
-const GoodsList = ({data}) => {
+const GoodsList = ({data, onDelete}) => {
   const elements = data.map(item => {
-    const {id, ...itemProps} = item;
+    const {id, ...itemProps} = item;    
     return ( 
-      <GoodsListItem key={id} {...itemProps}/> 
+      <GoodsListItem 
+        key={id}
+        {...itemProps}
+        onDelete={ () => onDelete(id) } /> 
     )
   });
   console.log(elements);
+
   return (
     <ul className="app-list list-group">{elements}</ul>
     )
