e71a42c3980ac2d7596ab15fc25f23bf12218e66
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-03-31 21:09:51 +0300

    Update app.js

--- REACT/lowest-price/src/components/app/app.js
+++ REACT/lowest-price/src/components/app/app.js
@@ -43,17 +43,40 @@ class App extends Component {
     super(props);
     this.state = {
       data: [
-        {brand: 'Colonist', type: 'portwein', volume: 0.7, price: 510, wish: false, id: 101},
-        {brand: 'Osborn', type: 'portwein', volume: 0.7, price: 300, wish: true, id: 102},
-        {brand: 'Colonist', type: 'brut', volume: 0.7, price: 360, wish: false, id: 103},
-        {brand: 'Martini', type:'brut', volume: 0.7, price: 245, wish: false, id: 104},
-        {brand: 'Espolon', type: 'yellow tequila', volume: 0.7, price:990, wish: false, id: 105},
-        {brand: 'Espolon', type: 'white tequila', volume: 0.7, price: 860, wish: false, id: 106},
-        {brand: 'Botafogo', type: 'amber rum', volume: 0.7, price: 700, wish: false, id: 107},
-        {brand: 'Jameson', type: 'whiskey', volume: 0.7, price: 500, wish: true, id: 108}
-      ]
+        {brand: 'Wild Turkey', type: 'bourbon', volume: '0.7L', price: 530, wish: false, star: false, id: 101},
+        {brand: 'Wild Turkey', type: 'bourbon', volume: '1.0L', price: 850, wish: false, star: false, id: 102},
+        {brand: 'Santiago Cuba', type: 'amber rum', volume: '0.7L', price: 750, wish: false, star: false, id: 103},
+        {brand: 'Doorly\'s', type: 'amber rum', volume: '0.7L', price: 725, wish: false, star: false, id: 104},
+        {brand: 'Botafogo', type: 'amber rum', volume: '0.7L', price: 700, wish: false, star: false, id: 105},
+        {brand: 'Appleton', type: 'amber rum', volume: '0.7L', price: 645, wish: false, star: false, id: 106},
+        {brand: 'Bacardi', type: 'amber rum', volume: '0.5L', price: 260, wish: false, star: false, id: 107},
+        {brand: 'Ballantines', type: 'whiskey Lime', volume: '0.7L', price: 550, wish: false, star: false, id: 108},
+        {brand: 'Jameson', type: 'whiskey', volume: '0.7L', price: 500, wish: true, star: false, id: 109},
+        {brand: 'Jameson', type: 'whiskey', volume: '0.5L', price: 470, wish: false, star: false, id: 110},
+        {brand: 'Ballantines', type: 'whiskey', volume: '0.7L', price: 490, wish: false, star: false, id: 111},
+        {brand: 'John Walker', type: 'whiskey', volume: '0.7L', price: 430, wish: false, star: false, id: 112},
+        {brand: 'John Walker', type: 'whiskey', volume: '0.5L', price: 350, wish: false, star: false, id: 113},
+        {brand: 'Gordon\'s', type: 'gin', volume: '0.7L', price: 450, wish: false, star: false, id: 114},
+        {brand: 'Colonist', type: 'portwein', volume: '0.7L', price: 510, wish: false, star: true, id: 115},
+        {brand: 'Osborn', type: 'portwein', volume: '0.7L', price: 300, wish: true, star: false, id: 116},
+        {brand: 'Colonist', type: 'champagne brut', volume: '0.7L', price: 360, wish: false, star: false, id: 117},
+        {brand: 'Martini', type:'champagne brut', volume: '0.7L', price: 245, wish: false, star: false, id: 118},
+        {brand: 'Espolon', type: 'yellow tequila', volume: '0.7L', price:990, wish: false, star: false, id: 119},
+        {brand: 'Espolon', type: 'white tequila', volume: '0.7L', price: 860, wish: false, star: false, id: 120},
+        {brand: 'Doorly\'s', type: 'white rum', volume: '0.7L', price: 645, wish: false, star: false, id: 121},
+        {brand: 'Santiago Cuba', type: 'white rum', volume: '0.7L', price: 420, wish: false, star: false, id: 122},
+        {brand: 'Jagermeister', type: 'tincture', volume: '0.7L', price: 460, wish: false, star: false, id: 123},
+        {brand: 'Jagermeister', type: 'tincture', volume: '1.0L', price: 600, wish: false, star: false, id: 124},
+        {brand: 'Becherovka', type: 'tincture', volume: '0.7L', price: 360, wish: false, star: false, id: 125},
+        {brand: 'Becherovka', type: 'tincture', volume: '0.5L', price: 250, wish: false, star: false, id: 126},
+        {brand: 'R.Jelinek', type: 'tincture', volume: '0.5L', price: 200, wish: false, star: false, id: 127},
+        {brand: 'Absolut', type: 'horilka', volume: '0.7L', price: 320, wish: false, star: false, id: 128},
+        {brand: 'Finlandia', type: 'horilka', volume: '0.7L', price: 300, wish: false, star: false, id: 129},
+        {brand: 'Finlandia', type: 'horilka', volume: '0.5L', price: 290, wish: false, star: false, id: 130}
+      ],
+      term: ''
     }
-      this.maxId = 109;
+      this.maxId = 131;
   }
   deleteItem = (id) => {
     this.setState( ({data}) => {
@@ -68,6 +91,7 @@ class App extends Component {
         volume, 
         price,
         wish: false,
+        star: false,
         id: this.maxId++
       }
       this.setState( ({data}) => {
@@ -75,22 +99,73 @@ class App extends Component {
           return { data: newArr }
       } );
   }
+/*
+  onToggleWish = (id) => {
+    this.setState( ({data}) => {
+      const index = data.findIndex(elem => elem.id === id);
+      const oldObj = data[index];
+      const newObj = {...oldObj, wish: !oldObj.wish};
+      const newArr = [...data.slice(0, index), newObj, ...data.slice(index + 1)];
+        return {
+          data: newArr
+        }
+    } ) 
+  }
+*/
+  onToggleProp = (id, prop) => {
+    this.setState( ({data}) => ({
+    // we must return new array, without mutating previous one  
+      data: data.map(item => {
+        if (item.id === id) { return {...item, [prop]: !item[prop]} }
+        return item;
+      })
+    }) )
+  }
+  searchEmp = (items, term) => {
+    if (term.length === 0) { return items;
+    }
+    return items.filter(item => {
+        return item.name.indexOf(term) > -1
+    })
+  }
+  onUpdateSearch = (term) => {
+    this.setState({term});
+  }
+  filterPost = (items, filter) => {
+    switch (filter) {
+        case 'rise':
+            return items.filter(item => item.rise);
+        case 'moreThen1000':
+            return items.filter(item => item.salary > 1000);
+        default:
+            return items
+    }
+  }
+  onFilterSelect = (filter) => {
+    this.setState({filter});
+  }
 
   render() {
+    const {data, term, filter} = this.state;
+    const goods = this.state.data.length;
+    const wished = this.state.data.filter(item => item.wish).length;
+    const visibleData = this.filterPost(this.searchEmp(data, term), filter);
+
     return (
       <div className="app">
-        <AppInfo />
+        <AppInfo goods={goods} wished={wished} />
 
         <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
+            <SearchPanel onUpdateSearch={this.onUpdateSearch} />
+            <AppFilter filter={filter} onFilterSelect={this.onFilterSelect} />
         </div>
         
         <GoodsList 
-          data={this.state.data}
-          onDelete={this.deleteItem}/>
+          data={visibleData}
+          onDelete={this.deleteItem}
+          onToggleProp={this.onToggleProp} />
         
-        <GoodsAddForm onAdd={this.addItem}/>
+        <GoodsAddForm onAdd={this.addItem} />
       </div>
     );
   }
