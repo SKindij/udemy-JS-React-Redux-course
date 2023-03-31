f9f922f99938ed4c58bf323d74a439b1cb55a6b8
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-03-31 22:30:56 +0300

    Update app-filter.js

--- REACT/lowest-price/src/components/app-filter/app-filter.js
+++ REACT/lowest-price/src/components/app-filter/app-filter.js
@@ -1,11 +1,36 @@
 import "./app-filter.css";
 
-const AppFilter = () => {
+const AppFilter = (props) => {
+  const buttonsData = [
+    {name: 'all', label: 'Усі товари'},
+    {name: 'stars', label: 'Зірковий вибір'},
+    {name: 'lessThen450', label: 'Ціна менше 450UAH'},
+    {name: 'lessThen700', label: 'Ціна менше 700UAH'}
+  ];
+  // on basis of data array, form set of buttons
+  const buttons = buttonsData.map( ({name, label}) => {
+    const active = props.filter === name;
+    const clazz = active ? 'btn-light' : 'btn-outline-light';
+    return (
+        <button type="button" className={`btn ${clazz}`}
+                key={name}
+                onClick={ () => props.onFilterSelect(name) }>
+                {label}
+        </button>
+    )
+  } )
+/*
   return (
     <div className="btn-group">
       <button type="button" className="btn btn-light">Усі товари</button>
-      <button type="button" className="btn btn-outline-light">Претендент в топ</button>
+      <button type="button" className="btn btn-outline-light">Зірковий вибір</button>
       <button type="button" className="btn btn-outline-light">Ціна більше 600UAH</button>
+    </div>
+  )
+*/
+  return (
+    <div className="btn-group">
+      {buttons}
     </div>
   )
 }
