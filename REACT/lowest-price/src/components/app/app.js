4cedabed1a673267b4c5e803859fc8cc3ef4ccb8
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-03-31 22:31:47 +0300

    Update app.js

--- REACT/lowest-price/src/components/app/app.js
+++ REACT/lowest-price/src/components/app/app.js
@@ -74,7 +74,8 @@ class App extends Component {
         {brand: 'Finlandia', type: 'horilka', volume: '0.7L', price: 300, wish: false, star: false, id: 129},
         {brand: 'Finlandia', type: 'horilka', volume: '0.5L', price: 290, wish: false, star: false, id: 130}
       ],
-      term: ''
+      userTerm: '',
+      filter: 'all'
     }
       this.maxId = 131;
   }
@@ -121,35 +122,40 @@ class App extends Component {
       })
     }) )
   }
-  searchEmp = (items, term) => {
-    if (term.length === 0) { return items;
-    }
+  searchEmp = (items, userTerm) => {
+    // when user has not entered anything in search field
+    if (userTerm.length === 0) { return items; }
     return items.filter(item => {
-        return item.name.indexOf(term) > -1
+      // select elements according to coincidence of term
+        return item.type.indexOf(userTerm) > -1
     })
   }
-  onUpdateSearch = (term) => {
-    this.setState({term});
+  onUpdateSearch = (userTerm) => {
+    this.setState({userTerm});
   }
   filterPost = (items, filter) => {
     switch (filter) {
-        case 'rise':
-            return items.filter(item => item.rise);
-        case 'moreThen1000':
-            return items.filter(item => item.salary > 1000);
+        case 'stars':
+            return items.filter(item => item.star);
+        case 'lessThen450':
+            return items.filter(item => item.price <= 450);
+        case 'lessThen700':
+            return items.filter(item => item.price <= 700);
         default:
             return items
     }
   }
+  // method for dynamic filtering of goods
   onFilterSelect = (filter) => {
     this.setState({filter});
   }
 
   render() {
-    const {data, term, filter} = this.state;
+    const {data, userTerm, filter} = this.state;
     const goods = this.state.data.length;
     const wished = this.state.data.filter(item => item.wish).length;
-    const visibleData = this.filterPost(this.searchEmp(data, term), filter);
+    // display data (goods) after double filtering
+    const visibleData = this.filterPost( this.searchEmp(data, userTerm), filter );
 
     return (
       <div className="app">
