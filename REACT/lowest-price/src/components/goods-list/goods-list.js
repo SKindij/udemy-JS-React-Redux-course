35ec1560488fea745941cd7c223ef36ad9d08a87
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-03-31 21:06:16 +0300

    Update goods-list.js

--- REACT/lowest-price/src/components/goods-list/goods-list.js
+++ REACT/lowest-price/src/components/goods-list/goods-list.js
@@ -2,14 +2,15 @@ import GoodsListItem from "../goods-list-item/goods-list-item";
 
 import './goods-list.css';
 
-const GoodsList = ({data, onDelete}) => {
+const GoodsList = ({data, onDelete, onToggleProp}) => {
   const elements = data.map(item => {
     const {id, ...itemProps} = item;    
     return ( 
       <GoodsListItem 
         key={id}
         {...itemProps}
-        onDelete={ () => onDelete(id) } /> 
+        onDelete={ () => onDelete(id) }
+        onToggleProp={ (e) => onToggleProp(id, e.currentTarget.getAttribute('data-toggle')) } /> 
     )
   });
   console.log(elements);
