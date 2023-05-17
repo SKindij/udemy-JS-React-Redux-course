86f9312b688344e26d89932b30f3d3de1ab342b6
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-05-17 21:18:40 +0300

    Update MarvelService.js

--- REACT/React-Hooks/src/services/MarvelService.js
+++ REACT/React-Hooks/src/services/MarvelService.js
@@ -1,39 +1,39 @@
-class MarvelService {
-  _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+// імпортуємо наш хук
+import { useHttp } from "../hooks/http.hook";
+
+const useMarvelService = () => {
+  // користуємося деструктуризацією
+  const { loading, request, error, clearError } = useHttp();
+
+
+
+
+
+  const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
   // public key
-  _apiKey = 'apikey=7021a063d0296193d0fe45e71f4cd3a1';
-  _baseOffset = 210;
+  const _apiKey = 'apikey=7021a063d0296193d0fe45e71f4cd3a1';
+  const _baseOffset = 210;
 
-// helper function that can be used to fetch data from an API endpoint  
-  getResource = async (url) => { // asynchronous arrow function that takes in a url parameter
-    // 'await' is used to wait for response to come back before moving on to next line of code
-      // 'fetch' send GET request to specified URL
-    let res = await fetch(url);  // response object is assigned to 'res' variable
-      // this 'if' statement checks whether response status code is not in 200-299 range
-      if (!res.ok) { throw new Error(`Could not fetch ${url}, status: ${res.status}`); } 
-        // 'await' is used to wait for JSON parsing to complete before returning data
-        return await res.json();
-  }
-/* 
-   In summary, 'getResource' function fetches data from specified API endpoint, 
-   checks response status code for errors, and returns parsed JSON data if request is successful.
-*/
-
-  getAllCharacters = async (offset = this._baseOffset) => {
+const getAllCharacters = async (offset = _baseOffset) => {
     // code from block 'INTERACTIVE API TESTER'
-    const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
+    const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`);
     // data from API "data": { "results": [...] }
-    return res.data.results.map(this._transformCharacter);
+    return res.data.results.map(_transformCharacter);
   }
 
-  getCharacter = async (id) => {
+  const getCharacter = async (id) => {
     // code from block 'INTERACTIVE API TESTER'
-    const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
     // data from API "data": { "results": [...] }
-    return this._transformCharacter(res.data.results[0]);    
+    return _transformCharacter(res.data.results[0]);    
   }
 
-  _transformCharacter = (char) => {
+
+
+
+
+
+  const _transformCharacter = (char) => {
     // data from API "data": { "results": [...] }
     return {
       id: char.id,
@@ -44,7 +44,15 @@ class MarvelService {
       wiki: char.urls[1].url,
       comics: char.comics.items
     }
-  }
-}
+  };
 
-export default MarvelService;
+  return {
+		loading,
+		error,
+		clearError,
+		getAllCharacters,
+		getCharacter,
+	};
+};
+
+export default useMarvelService;
