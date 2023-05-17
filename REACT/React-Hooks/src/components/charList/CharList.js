c13353b4cd31af09ac3ef061c6b180a490bc0a8d
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-05-17 21:25:57 +0300

    Update CharList.js

--- REACT/React-Hooks/src/components/charList/CharList.js
+++ REACT/React-Hooks/src/components/charList/CharList.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
-import MarvelService from '../../services/MarvelService';
+import useMarvelService from '../../services/MarvelService';
 import './charList.scss';
 
 const CharList = (props) => {
@@ -15,13 +15,12 @@ const CharList = (props) => {
   const [offset, setOffset] = useState(210);
   const [charEnded, setCharEnded] = useState(false);
 
-  const marvelService = new MarvelService();
+  const marvelService = useMarvelService();
 
   // hook замість componentDidMount()
   useEffect(() => {
     onRequest();
   }, [])
-
 
   const onRequest = (offset) => {
     onCharListLoading();
