387e9234f070dace0d7a9a7cc3cb0ea8a2804958
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-05-17 21:53:36 +0300

    Update RandomChar.js

--- REACT/React-Hooks/src/components/randomChar/RandomChar.js
+++ REACT/React-Hooks/src/components/randomChar/RandomChar.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState, useEffect } from 'react';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -8,43 +9,29 @@ import mjolnir from '../../resources/img/mjolnir.png';
 
 const RandomChar = () => {
   const [char, setChar] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
-
-  const marvelService = useMarvelService();
+  // стани беремо із нашого кастомного хука
+  const {loading, error, getCharacter, clearError} = useMarvelService();
 
   // hooks
   useEffect(() => {
     updateChar();
-    const timerId = setInterval(updateChar, 60000);
-
+    const timerId = setInterval(updateChar, 55000);
     return () => {
         clearInterval(timerId)
     }
   }, [])
 
-const onCharLoaded = (char) => {
-    setLoading(false);
+  const onCharLoaded = (char) => {
     setChar(char);
-}
-
-const onCharLoading = () => {
-    setLoading(true);
-}
-
-const onError = () => {
-    setError(true);
-    setLoading(false);
-}
+  }
 
   // in order not to lose context, use arrow function
-const updateChar = () => { 
+  const updateChar = () => { 
+    clearError();
     const id = Math.floor( Math.random() * (1011400 - 1011000) + 1011000 );
-    onCharLoading();
-    marvelService.getCharacter(id)
-        .then(onCharLoaded)
-        .catch(onError);
-}
+    getCharacter(id)
+      .then(onCharLoaded)
+  }
 
     const errorMessage = error ? <ErrorMessage /> : null;
     const spinner = loading ? <Spinner /> : null;
@@ -72,6 +59,9 @@ const updateChar = () => {
 
 // this component displays left half of character block
 const View = ({char}) => {
+  if (!char) {
+    return null; // Повертаємо null, якщо char є null
+  }
   // data from API "data": { "results": [...] }
   const {name, description, thumbnail, homepage, wiki} = char;
     let imgStyle = {'objectFit' : 'cover'};
