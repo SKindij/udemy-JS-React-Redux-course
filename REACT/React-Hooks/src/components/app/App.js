c60c47ab75be35857c539630ac537b412961068f
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-05-20 21:59:39 +0300

    Update App.js

--- REACT/React-Hooks/src/components/app/App.js
+++ REACT/React-Hooks/src/components/app/App.js
@@ -1,27 +1,23 @@
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+// із  таким видом імпорту webpack шукає в папці index.js
 import {MainPage, ComicsPage} from '../pages';
 import AppHeader from "../appHeader/AppHeader";
 
 const App = () => {
     
-    return (
-        <Router>
-            <div className="app">
-                <AppHeader/>
-                <main>
-                    <Switch>
-                        <Route exact path="/">
-                            <MainPage/>
-                        </Route>
-                        <Route exact path="/comics">
-                            <ComicsPage/>
-                        </Route>
-                    </Switch>
-                </main>
-            </div>
-        </Router>
-    )
+  return (
+    <Router>
+      <div className="app">
+        <AppHeader/>
+        <main>
+          <Routes>
+            <Route exact path="/" element={<MainPage/>} />
+            <Route exact path="/comics" element={<ComicsPage/>} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
