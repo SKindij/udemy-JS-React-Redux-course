0d219f244325b0578697d5a0fc4b7c5fd559746a
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-05-20 23:38:38 +0300

    Update App.js

--- REACT/React-Hooks/src/components/app/App.js
+++ REACT/React-Hooks/src/components/app/App.js
@@ -13,7 +13,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<MainPage/>} />
             <Route path="/comics" element={<ComicsPage/>} />
-            <Route path="/comics/:comicId" element={SingleComicPage} />
+            <Route path="/comics/:comicId" element={<SingleComicPage/>} />
             <Route path="*" element={<Page404/>} />
           </Routes>
         </main>
