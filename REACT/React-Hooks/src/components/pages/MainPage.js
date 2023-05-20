7e9956f67a821c5a550c8b5bc2261059e07f23e8
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-05-20 21:04:30 +0300

    Update MainPage.js

--- REACT/React-Hooks/src/components/pages/MainPage.js
+++ REACT/React-Hooks/src/components/pages/MainPage.js
@@ -1,3 +1,9 @@
+import { useState } from "react";
+
+import RandomChar from "../randomChar/RandomChar";
+import CharList from "../charList/CharList";
+import CharInfo from "../charInfo/CharInfo";
+import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 import decoration from '../../resources/img/vision.png';
 
