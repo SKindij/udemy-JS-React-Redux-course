26a46901570556701b75b7b4d751eb1e08e5ae64
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-05-21 14:11:28 +0300

    Update App.js

--- REACT/React-Hooks/src/components/app/App.js
+++ REACT/React-Hooks/src/components/app/App.js
@@ -1,21 +1,32 @@
+import {lazy, Suspense} from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-// із  таким видом імпорту webpack шукає в папці index.js
-import {MainPage, ComicsPage, Page404, SingleComicPage} from '../pages';
+
 import AppHeader from "../appHeader/AppHeader";
+import Spinner from '../spinner/Spinner';
+
+// із  таким видом імпорту webpack шукає в папці index.js
+import {MainPage, ComicsPage} from '../pages';
+
+// динамічні імпорти ставимо після статичних імпортів
+const Page404 = lazy( () => import('../pages/404') );
+const SingleComicPage = lazy( () => import('../pages/SingleComicPage') );
 
 const App = () => {
-    
+  // fallback - запасний компонент на випадок проблем завантаження
+  
   return (
     <Router>
       <div className="app">
         <AppHeader/>
         <main>
-          <Routes>
-            <Route path="/" element={<MainPage/>} />
-            <Route path="/comics" element={<ComicsPage/>} />
-            <Route path="/comics/:comicId" element={<SingleComicPage/>} />
-            <Route path="*" element={<Page404/>} />
-          </Routes>
+          <Suspense fallback={<Spinner/>}>
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/comics" element={<ComicsPage />} />
+              <Route path="/comics/:comicId" element={<SingleComicPage />} />
+              <Route path="*" element={<Page404 />} />
+            </Routes>
+          </Suspense>
         </main>
       </div>
     </Router>
