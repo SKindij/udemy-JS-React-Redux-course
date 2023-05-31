6036f128c48fd6c24d1edcdad83aa86a2fd7d6be
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-05-31 13:52:28 +0300

    refactor: optimize performance without changing overall behavior

--- REACT/React-Hooks/src/components/pages/singleCharacterLayout/SingleCharacterLayout.js
+++ REACT/React-Hooks/src/components/pages/singleCharacterLayout/SingleCharacterLayout.js
@@ -1,62 +1,18 @@
-import { useParams, Link } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import './singleCharacterLayout.scss';
 
-import useMarvelService from '../../services/MarvelService';
-import Spinner from '../spinner/Spinner';
-import ErrorMessage from '../errorMessage/ErrorMessage';
-import AppBanner from "../appBanner/AppBanner";
-import './singleComicPage.scss';
+const SingleCharacterLayout = ({data}) => {
 
-const SingleComicPage = () => {
-  const {comicId} = useParams();
-  const [comic, setComic] = useState(null);
-  const {loading, error, getComic, clearError} = useMarvelService();
+    const {name, description, thumbnail} = data;
 
-  // якщо користувач в полі браузера змінить id комікса, то сторінка оновиться
-  useEffect( () => {
-    updateComic();
-  }, [comicId] )
-
-  const updateComic = () => {
-    clearError();
-    getComic(comicId)
-    .then(onComicLoaded);
-  }
-
-  const onComicLoaded = (comic) => {
-    setComic(comic);
-  }
-
-  const errorMessage = error ? <ErrorMessage/> : null;
-  const spinner = loading ? <Spinner/> : null;
-  const content = !(loading || error || !comic) ? <View comic={comic}/> : null;
-
-  return (
-      <>
-        <AppBanner/>
-        {errorMessage}
-        {spinner}
-        {content}
-      </>
+    return (
+        <div className="single-comic">
+            <img src={thumbnail} alt={name} className="single-comic__char-img"/>
+            <div className="single-comic__info">
+                <h2 className="single-comic__name">{name}</h2>
+                <p className="single-comic__descr">{description}</p>
+            </div>
+        </div>
     )
 }
 
-const View = ({comic}) => {
-  const {title, description, pageCount, thumbnail, language, price} = comic;
-
-  return (
-    <div className="single-comic">
-        <img src={thumbnail} alt={title} className="single-comic__img"/>
-        <div className="single-comic__info">
-            <h2 className="single-comic__name">{title}</h2>
-            <p className="single-comic__descr">{description}</p>
-            <p className="single-comic__descr">{pageCount}</p>
-            <p className="single-comic__descr">Language: {language}</p>
-            <div className="single-comic__price">{price}</div>
-        </div>
-        <Link to="/comics" className="single-comic__back">Back to all</Link>
-    </div>
-  )
-}
-
-export default SingleComicPage;
+export default SingleCharacterLayout;
