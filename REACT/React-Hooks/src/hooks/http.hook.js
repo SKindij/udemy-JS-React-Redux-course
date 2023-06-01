a98acb3f8fe68bf826f49d8d489feafd1f82eed9
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-06-01 12:45:02 +0300

    feat: add states for FSM
    incorporate Finite state machine

--- REACT/React-Hooks/src/hooks/http.hook.js
+++ REACT/React-Hooks/src/hooks/http.hook.js
@@ -4,10 +4,13 @@ import { useState, useCallback } from "react";
 export const useHttp = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  // встановлюємо початковий стан - "очікування"
+  const [process, setProcess] = useState('waiting');
 
   const request = useCallback( async (url, method = 'GET', body = null, headers = {'Content-Type': 'application/json'}) => {
     // одразу вмикаємо покажчик завантаження
     setLoading(true);
+    setProcess('loading');
 
     try {
       const response = await fetch( url, {method, body, headers} );
@@ -19,15 +22,20 @@ export const useHttp = () => {
       const data = await response.json();
       // завантаження завершилось
       setLoading(false);
+      setProcess('confirmed');
         return data;
     } catch(e) {
         setLoading(false);
         setError(e.message);
+        setProcess('error');
         throw e;
     }
   }, [] );
   // очищуємо паамять про помилку, щоб можна було працювати длі
-  const clearError = useCallback( () => setError(null), [] );
+  const clearError = useCallback( () => {
+    setError(null);
+    setProcess('loading');
+  }, [] );
 
-  return { loading, request, error, clearError }
+  return { loading, request, error, clearError, process }
 }
