1f2d9de42e67bad105e171d1a2df36af7ae5681d
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-06-02 21:36:20 +0300

    feat: introduce new feature to codebase
    update HeroesList.js
    add option to delete heroes

--- Redux/Hero-Admin-Panel/src/components/heroesList/HeroesList.js
+++ Redux/Hero-Admin-Panel/src/components/heroesList/HeroesList.js
@@ -1,28 +1,43 @@
 import {useHttp} from '../../hooks/http.hook';
-import { useEffect } from 'react';
+import { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { CSSTransition, TransitionGroup} from 'react-transition-group';
 
-import { heroesFetching, heroesFetched, heroesFetchingError } from '../../actions';
+import { heroesFetching, heroesFetched, heroesFetchingError, heroDeleted } from '../../actions';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
 
-
-// При клике на "крестик" идет удаление персонажа из общего состояния
-// Усложненная задача:
-// Удаление идет и с json файла при помощи метода DELETE
+import './heroesList.scss';
 
 const HeroesList = () => {
-  const {heroes, heroesLoadingStatus} = useSelector(state => state);
+  // хук, що дозволяє витягувати шматочки глобального стейту для цього компоненту
+  const {filteredHeroes, heroesLoadingStatus} = useSelector(state => state);
   const dispatch = useDispatch();
+  // хук, що дозволяє отримти обьєкт запиту
   const {request} = useHttp();
 
-  useEffect(() => {
-    dispatch(heroesFetching());
+  // використовуємо хук, щоб зробити зпит у потрібний час
+  useEffect( () => {
+    dispatch( heroesFetching() );
+    // робимо запит героїв на фейк API
     request("http://localhost:3001/heroes")
-        .then( data => dispatch(heroesFetched(data)) )
-        .catch( () => dispatch(heroesFetchingError()) )
-        // eslint-disable-next-line
-    }, []);
+      // обробляємо отриманий проміс
+        .then( data => dispatch( heroesFetched(data) ) )
+        .catch( () => dispatch( heroesFetchingError() ) )
+    // eslint-disable-next-line
+    }, [] );
+
+  // функція бере id і по ньому видаляє непотрібного персонажа зі store
+  // ТІЛЬКИ якщо запит на видалення пройшов успішно
+  // відстежуйте ланцюжок дій actions => reducers
+  const onDelete = useCallback( (id) => {
+    // видалення персонажа за його id із бази даних
+    request(`http://localhost:3001/heroes/${id}`, "DELETE")
+      .then(data => console.log(data, 'Deleted'))
+      .then(dispatch(heroDeleted(id)))
+      .catch(err => console.log(err));
+    // eslint-disable-next-line  
+  }, [request] );
 
   if (heroesLoadingStatus === "loading") {
     return <Spinner />;
@@ -31,20 +46,28 @@ const HeroesList = () => {
   }
 
   const renderHeroesList = (arr) => {
+    // якщо героїв немає в базі даних
     if (arr.length === 0) {
-        return <h5 className="text-center mt-5">Героїв поки що немає</h5>
+      return (
+        <CSSTransition timeout={0} classNames="hero">
+          return <h5 className="text-center mt-5">Героїв поки що немає</h5>
+        </CSSTransition>
+      )    
     }
-
     return arr.map( ({id, ...props}) => {
-        return <HeroesListItem key={id} {...props}/>
+      return (
+        <CSSTransition key={id} timeout={500} classNames="hero">
+          <HeroesListItem {...props} onDelete={() => onDelete(id)} />
+        </CSSTransition>
+      )        
     })
   }
+  const elements = renderHeroesList(filteredHeroes);
 
-  const elements = renderHeroesList(heroes);
   return (
-        <ul>
-            {elements}
-        </ul>
+    <TransitionGroup component="ul">
+      {elements}
+    </TransitionGroup>
   )
 }
 
