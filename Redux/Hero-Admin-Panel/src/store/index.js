3e880f917229902170ff53abbd497a5a580e6082
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-06-03 13:14:12 +0300

    feat: introduce new feature to codebase
    use combineReducers, compose

--- Redux/Hero-Admin-Panel/src/store/index.js
+++ Redux/Hero-Admin-Panel/src/store/index.js
@@ -1,8 +1,26 @@
-import { legacy_createStore as createStore, combineReducers } from 'redux';
+import { legacy_createStore as createStore, combineReducers, compose } from 'redux';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
-const store = createStore( combineReducers({heroes, filters}),
-     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() );
+const enhancer = (createStore) => (...args) => {
+    const store = createStore(...args);
+    // зберігаємо наш оригінальний dispatch, що міг приймати лише обьєкти
+    const oldDispatch = store.dispatch;
+    // перезаписуємо його із функцією прийому строки
+    store.dispatch = (action) => {
+        if (typeof action === 'string') {
+            return oldDispatch({type: action})
+        }
+        return oldDispatch(action);
+    }
+    return store;
+}
+
+const store = createStore( 
+    combineReducers({heroes, filters}),
+    compose(
+      enhancer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() 
+    ) );
 
 export default store;
