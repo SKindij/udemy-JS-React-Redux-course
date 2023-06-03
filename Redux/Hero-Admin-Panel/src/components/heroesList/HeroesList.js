c4148fabb4f4ed61bea65dea0789aed6c5292a60
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-06-03 10:36:01 +0300

    refactor: optimize performance
    Update HeroesList.js

--- Redux/Hero-Admin-Panel/src/components/heroesList/HeroesList.js
+++ Redux/Hero-Admin-Panel/src/components/heroesList/HeroesList.js
@@ -2,6 +2,7 @@ import {useHttp} from '../../hooks/http.hook';
 import { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { CSSTransition, TransitionGroup} from 'react-transition-group';
+import { createSelector } from 'reselect';
 
 import { heroesFetching, heroesFetched, heroesFetchingError, heroDeleted } from '../../actions';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
@@ -10,8 +11,24 @@ import Spinner from '../spinner/Spinner';
 import './heroesList.scss';
 
 const HeroesList = () => {
-  // хук, що дозволяє витягувати шматочки глобального стейту для цього компоненту
-  const {filteredHeroes, heroesLoadingStatus} = useSelector(state => state);
+  const filteredHeroesSelector = createSelector(
+    (state) => state.filters.activeFilter,
+    (state) => state.heroes.heroes,
+    (filter, heroes) => {
+        if (filter === 'all') {
+            console.log('render all');
+            return heroes;
+        } else {
+            // створюємо новий відфільтрований масив
+            return heroes.filter(item => item.element === filter);
+        }
+    }
+  );
+  
+  // хук useSelector дозволяє витягувати шматочки глобального стейту для цього компоненту
+  const filteredHeroes = useSelector(filteredHeroesSelector);
+  const heroesLoadingStatus = useSelector(state => state.heroesLoadingStatus);
+
   const dispatch = useDispatch();
   // хук, що дозволяє отримти обьєкт запиту
   const {request} = useHttp();
@@ -27,15 +44,15 @@ const HeroesList = () => {
     // eslint-disable-next-line
     }, [] );
 
-  // функція бере id і по ньому видаляє непотрібного персонажа зі store
-  // ТІЛЬКИ якщо запит на видалення пройшов успішно
+  // функція бере id і по ньому видаляє непотрібного персонажа зі store  
   // відстежуйте ланцюжок дій actions => reducers
   const onDelete = useCallback( (id) => {
     // видалення персонажа за його id із бази даних
     request(`http://localhost:3001/heroes/${id}`, "DELETE")
-      .then(data => console.log(data, 'Deleted'))
-      .then(dispatch(heroDeleted(id)))
-      .catch(err => console.log(err));
+      .then( data => console.log(data, 'Deleted') )
+  // ТІЛЬКИ якщо запит до сервера на видалення пройшов успішно
+      .then( dispatch(heroDeleted(id)) )
+      .catch( err => console.log(err) );
     // eslint-disable-next-line  
   }, [request] );
 
@@ -50,7 +67,7 @@ const HeroesList = () => {
     if (arr.length === 0) {
       return (
         <CSSTransition timeout={0} classNames="hero">
-          return <h5 className="text-center mt-5">Героїв поки що немає</h5>
+          <h5 className="text-center mt-5">Героїв поки що немає</h5>
         </CSSTransition>
       )    
     }
